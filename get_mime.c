21fff279801f81ae4a1cd88957b404404c318de1
Edvard Halmrast Lentz <edvardhlentz@gmail.com>
2020-01-09 13:23:40 +0000

    Laget en funksjon for å finne mime til fil

---
+++ get_mime.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+
+#define MIMEFILE "./etc/mime.styles"
+
+char* get_mime(char* path)
+{
+  // get file name
+  const char* file = strrchr(path, '/') + 1;
+  
+  // get file extension
+  const char* dot = strrchr(file, '.');
+  if(!dot || dot == file) dot = " ";
+  dot++;
+  
+  // open mime file
+  FILE* mimes = fopen(MIMEFILE, "r");
+  char* txt = NULL;
+  size_t len;
+
+  // read mime file
+  while(len = getline(&txt, &len, mimes))
+  {
+    if(strstr(dot, strchr(txt, ' ')))
+    {
+      fclose(mimes);
+      return strtok(txt, "\t");
+    }
+  }
+  fclose(mimes);
+  return NULL;
+}
