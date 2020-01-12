bb1d66b50af6277b41f51528521dc49697a63d41
Lars Rustand <rustand.lars@gmail.com>
2020-01-12 15:25:54 +0100

    Fikse directory listing funksjonen

--- directory_listing.c
+++ directory_listing.c
@@ -11,10 +11,9 @@ void directory_listing(char *path){
   struct dirent    *ent;
   DIR              *dir;
 
-  if ((dir = opendir (path)) == NULL) {
+  dir = opendir (path);
+  if (dir == NULL) {
     perror (""); exit(1); }
-
-  chdir(path);
 
   printf ("\nKatalogen %s:\n", path);
   printf ("------------------------------------\n");
@@ -22,9 +21,6 @@ void directory_listing(char *path){
   printf ("------------------------------------\n");
 
   while ((ent = readdir (dir)) != NULL) {
-
-    if (stat (ent->d_name, &stat_buffer) < 0) {
-      perror(""); exit(2); }
 
     printf ("%o\t\t", stat_buffer.st_mode & 0777 );
     printf ("%d\t",   stat_buffer.st_uid);
