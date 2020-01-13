0e97427d2b93b7e3e67e417ccbc3124c7983e6b0
Lars Rustand <rustand.lars@gmail.com>
2020-01-13 12:52:20 +0100

    Fikser lesing av binære filer

--- print_file.c
+++ print_file.c
@@ -3,11 +3,21 @@ void print_file(char* path)
 {
   FILE* file = fopen(path, "r");
   size_t len = 0;
-  char* txt;
+  char *buffer;
+  long filelen;
 
-  while(getline(&txt, &len, file) != -1)
-  {
-    printf("%s", txt);
-  }
+
+  // Finn lengden av filen
+  fseek(file, 0, SEEK_END);
+  filelen = ftell(file);
+  rewind(file);
+
+  // Alloker minne og les fila
+  buffer = (char *)malloc((filelen)*sizeof(char));
+  fread(buffer, filelen, 1, file);
+  write(1,buffer,filelen);
+
+  // Lukk fila og frigjør minnet
   fclose(file);
+  free(buffer);
 }
