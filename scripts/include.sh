8a80919d13ad8fcaa761d95b4359831e500fc24b
Lars Rustand <rustand.lars@gmail.com>
2020-03-14 11:56:24 +0100

    Escaper lt og gt for å unngå injection

--- scripts/include.sh
+++ scripts/include.sh
@@ -10,7 +10,12 @@ urlDecode(){
 	echo "$1" | sed 's/+/ /g'
 }
 
+# Escaper < og > for å unngå html/javascript injection
+escapeLtGt(){
+	echo "$1" | sed -e 's/</\&lt;/g' -e 's/>/\&gt;/g'
+}
+
 # Formaterer dikt for visning. Decoder og unescaper spesialtegn
 formaterForVisning(){
-	urlDecode "$(fixBackslash $1)"
+	urlDecode "$(fixBackslash $(escapeLtGt $1))"
 }
